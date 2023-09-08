b6f8c25f8542d3ae8e67a0a47e2eb750d491f9d6
Alexander Evans <sander7989@gmail.com>
2023-09-07 23:01:02 -0600

    add test javascript database queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+let cohort = process.argv[2];
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${cohort || 'JUL02'}%'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
